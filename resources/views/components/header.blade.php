0b46db95499c6eb86f6f4b55596c438955bf34b9
Quintanamo <qherb@ycp.edu>
2023-06-16 13:14:29 -0400

    scss and components

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,4 @@
+<header class="header">
+    <h1 class="primary-header">Example Site</h1>
+    <x-navigation />
+</header>
---
+++ resources/views/components/navigation.blade.php
@@ -0,0 +1,8 @@
+@vite(['resources/scss/main.scss'])
+
+<nav class="nav">
+    <a class="nav__link" href="#">Link One</a>
+    <a class="nav__link" href="#">Link Two</a>
+    <a class="nav__link" href="#">Link Three</a>
+    <a class="nav__link" href="#">Link Four</a>
+</nav>
